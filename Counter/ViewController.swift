99361f79f61d211d529106dc52aac26b4fcb45a8
Michael Cheung <michaelcheung@Michaels-MacBook-Pro-3.local>
2017-10-08 17:42:52 +0800

    First commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Michael Cheung on 8/10/2017.
+//  Copyright © 2017 Michael Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBAction func plusOne(_ sender: Any) {
+    }
+    @IBAction func minusOne(_ sender: Any) {
+    }
+    @IBAction func resetFunc(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
